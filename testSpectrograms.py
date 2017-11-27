7edf278c1e4f21c8d47fdf084c5283529b35dc0a
charlie1329 <charlie.street1@btinternet.com>
2017-11-27 12:27:46 +0000

    Done tests

--- testSpectrograms.py
+++ testSpectrograms.py
@@ -10,16 +10,18 @@ import sys
 # function for computing stft and then outputting the spectrogram
 # input_path is the wav file to read from (the test file)
 # ground_truth is the original guitar stem
-def showSpectrogram(input_path, ground_truth):
+def compareSpectrograms(input_path, ground_truth, segment_size, overlap):
 	
-	segment_size = 8192
-	overlap = 7680
+	print('Starting...')
 
 	# get the test spectrogram
 
 	rate, data = wav.read(input_path)
 
 	data = data.astype(float)
+	if(len(data.shape) > 1 and data.shape[1] == 2):
+		data = data.T
+		data = (data[0] + data[1])/2.0
 
 	f, t, Zxx = sig.stft(data,fs=rate,window='hann',nperseg=segment_size,noverlap=overlap)
 
@@ -41,11 +43,51 @@ def showSpectrogram(input_path, ground_truth):
 
 	lms = 0.0
 
-	for i in range(0,gtMat.shape[0]):
+	#get the index for the low cut off and start from there
+	bin_size = rate/segment_size
+	index = round(80/bin_size)
+	index2 = round(2000/bin_size)
+
+	print('Starting MSE calculation...')
+
+	print(gtMat.shape)
+	print(testMat.shape)
+
+	TP = 0
+	TN = 0
+	FP = 0
+	FN = 0
+
+	#threshold = 0.05
+
+	for i in range(0,index):
+		for j in range(0,testMat.shape[1]):
+			testMat[i,j] = 0
+
+	for i in range(0,index):
 		for j in range(0,gtMat.shape[1]):
+			gtMat[i,j] = 0
+
+	for i in range(index2,testMat.shape[0]):
+		for j in range(0,testMat.shape[1]):
+			testMat[i,j] = 0
+
+	for i in range(index2,gtMat.shape[0]):
+		for j in range(0,gtMat.shape[1]):
+			gtMat[i,j] = 0
+
+	testMat = (testMat - np.mean(testMat)) / np.std(testMat)
+	gtMat = (gtMat - np.mean(gtMat)) / np.std(gtMat)
+
+	
+	print("Here")
+
+	for i in range(index,index2):
+		print("New loop: " + str(i))
+		for j in range(0,min(testMat.shape[1],gtMat.shape[1])):
 			lms += pow(gtMat[i,j]-testMat[i,j],2.0)
 
-	lms /= totalElements # take average
+	lms /= ((index2-index) * min(testMat.shape[1],gtMat.shape[1])) # take average
 
 	print("The lms error is: " + str(lms))
 
@@ -53,9 +95,11 @@ def showSpectrogram(input_path, ground_truth):
 
 if __name__ == '__main__':
 	
-	if len(sys.argv) != 8:
+	if len(sys.argv) != 5:
 		print('Incorrect number of input arguments')
 	else:
 		input_path = sys.argv[1]
 		ground_truth = sys.argv[2]
-		compareSpectrograms(input_path,ground_truth)
+		segment_size = int(sys.argv[3])
+		overlap = int(sys.argv[4])
+		compareSpectrograms(input_path,ground_truth,segment_size,overlap)
